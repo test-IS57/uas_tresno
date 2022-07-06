0dc94b81fe58136fee8fb38b7ba966fa37768f6d
test-IS57 <is57lp3i@gmail.com>
2022-07-06 20:13:26 +0700

    buat migration table karyawan dan jabatan

--- database/migrations/2022_07_06_130723_create_jabatans_table.php
+++ database/migrations/2022_07_06_130723_create_jabatans_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('jabatans', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('hak_akses', ['admin', 'user']);
-            $table->rememberToken();
+            $table->string('kode')->unique();
+            $table->string('jabatan');
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('jabatans');
     }
 };
--- database/migrations/2022_07_06_130902_create_karyawans_table.php
+++ database/migrations/2022_07_06_130902_create_karyawans_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('karyawans', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('hak_akses', ['admin', 'user']);
-            $table->rememberToken();
+            $table->string('nik');
+            $table->string('nama');
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->string('alamat');
+            $table->bigInteger('jabatans_id');
             $table->timestamps();
         });
     }
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('karyawans');
     }
 };
